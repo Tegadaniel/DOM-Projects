e5712deabe166da59d718cdea6ab33f6aa8bf74f
Tega Daniel <tegadaniels8@gmail.com>
2020-04-24 15:52:08 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,72 @@
+//checking for submit
+document.getElementById('bmi-form').addEventListener('submit', function(e){
+    //Hide results
+    document.getElementById('results').style.display = 'none';
+
+    //show Loader
+    document.getElementById('loading').style.display = 'block';
+    setTimeout(calculateResults, 2000);
+
+    e.preventDefault();
+});
+
+//creating resuts
+function calculateResults(){
+  const height = document.getElementById('height');
+  const weight = document.getElementById('weight');
+  const result = document.getElementById('bmi-result');
+
+
+  const calWeight =(weight.value);
+  const calHeight = (height.value) / 100;
+
+
+  // Compute BMI
+  const x = Math.pow(calHeight, 2);
+  const BMI = calWeight/x;
+
+  if(isFinite(BMI)){
+    result.value = BMI.toFixed(2);
+      //show results
+      document.getElementById('results').style.display = 'block';
+
+      //Hide Loader
+      document.getElementById('loading').style.display = 'none';
+  }else{
+    displayError('Please enter numbers');
+  }
+
+
+}
+
+//create displayError
+function displayError(error){
+     //Hide results
+     document.getElementById('results').style.display = 'none';
+
+     //Hide Loader
+     document.getElementById('loading').style.display = 'none';
+
+  //creating Div
+  const errorDiv = document.createElement('div'); 
+
+  //Get Parent element and where you want your error to show
+  const card = document.querySelector('.card');
+  const heading = document.querySelector('.heading');
+
+  //Adding class to your Div
+  errorDiv.className = 'alert alert-danger';
+
+  //Create textnode and append to div
+  errorDiv.appendChild(document.createTextNode(error));
+
+  //Inserting above the heading
+  card.insertBefore(errorDiv, heading);
+
+  // Clear your error
+  setTimeout(clearError, 2000)
+}
+
+function clearError(){
+  document.querySelector('.alert').remove();
+}
